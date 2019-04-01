e02489704030fe1ffb707d1f8fe65a7770e41e18
Ruslan <ruslan.08.02@yandex.ru>
2019-04-01 15:39:42 +0300

    vue-awesome-swiper installed Altanative to v-carousel

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 import Vuetify from 'vuetify'
 import VueRouter from 'vue-router'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 import 'vuetify/dist/vuetify.min.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.use(Vuetify)
 Vue.use(VueRouter)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   el: '#app',
