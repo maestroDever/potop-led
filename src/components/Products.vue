c5fc4f82456e383e6f984bf5641a5f22fac769cf
Ruslan <ruslan.08.02@yandex.ru>
2019-04-01 15:40:35 +0300

    swiper test on components

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,37 @@
 <template>
     <v-container>
-        Products
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">I'm Slide {{ slide }}</swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
     </v-container>
 </template>
+
+<script>
+
+import 'swiper/dist/css/swiper.css'
+
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+    name: 'Products',
+    components: {
+        swiper,
+        swiperSlide
+    },
+    data() {
+        return {
+            swiperOption: {
+                pagination: {
+                    el: '.swiper-pagination'
+                },
+                autoplay: {
+                    delay: 900,
+                },
+            },
+            swiperSlides: [1, 2, 3, 4, 5]
+        }
+    },
+}
+
+</script>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,5 +1,58 @@
 <template>
-    <v-container>
-        Services
+    <v-container class="swiper-inner">
+        <swiper :options="swiperOption">
+            <swiper-slide>Slide 1</swiper-slide>
+            <swiper-slide>Slide 2</swiper-slide>
+            <swiper-slide>Slide 3</swiper-slide>
+            <swiper-slide>Slide 4</swiper-slide>
+            <swiper-slide>Slide 5</swiper-slide>
+            <swiper-slide>Slide 6</swiper-slide>
+            <swiper-slide>Slide 7</swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
     </v-container>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        swiperOption: {
+          effect: 'coverflow',
+          grabCursor: true,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true
+          },
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          autoplay: {
+            delay: 1500
+          },
+          speed: 1000
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .swiper-inner {
+    width: 100%;
+    height: 400px;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 300px;
+    height: 300px;
+  }
+</style>
